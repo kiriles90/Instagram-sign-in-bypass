c4404e9b1cbdc914c5c600bbb0b4917860f3d2c5
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2022-05-28 21:35:21 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram sign-in bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.5
 // @date         2022-05-28
 // @author       github.com/kiriles90
 // @updateURL    https://raw.githubusercontent.com/kiriles90/Instagram-sign-in-bypass/master/violentmonkey.js
@@ -12,26 +12,26 @@
 // @grant        none
 // ==/UserScript==
 window.setTimeout(function(){
-  const clone = document.querySelector('body main > div > div > div:nth-child(2) > div > p');
-  if (clone && clone.lastChild && clone.lastChild.href && clone.lastChild.href.includes('instagram.com/accounts/emailsignup')) {
-    const link = 'https://www.picuki.com/profile' + location.search.replace('?next=', '');
-    const button = clone.cloneNode(true);
-    button.innerHTML = `or ( ° ͜ʖ͡°)╭∩╮ Zuckerberg<br>and <a href="` + link + `"><span>open with Picuki</span></a> instead`;
-    clone.parentElement.appendChild(button);
-  } else {
-    (function() {
-      'use strict';
-      let observer = new MutationObserver((mutations) => {
-        mutations.forEach((m) => {
-          m.target.style.overflow = "scroll";
-          if (document.querySelector("body > div.RnEpo._Yhr4")) {
-            document.querySelector("body > div.RnEpo._Yhr4").remove();
-          }
-        });
-      });
-      observer.observe(document.querySelector("body"), {
-        attributes: true,
-      });
-    })();
-  }
+    const clone = document.querySelector('body main > div > div > div:nth-child(2) > div > p');
+    if (clone && clone.lastChild && clone.lastChild.href && clone.lastChild.href.includes('instagram.com/accounts/emailsignup')) {
+        const link = 'https://www.picuki.com/profile' + location.search.replace('?next=', '');
+        const button = clone.cloneNode(true);
+        button.innerHTML = `or ( ° ͜ʖ͡°)╭∩╮ Zuckerberg<br>and <a href="` + link + `"><span>open with Picuki</span></a> instead`;
+        clone.parentElement.appendChild(button);
+    } else {
+        (function() {
+            'use strict';
+            let observer = new MutationObserver((mutations) => {
+                mutations.forEach((m) => {
+                    m.target.style.overflow = "scroll";
+                    if (document.querySelector("body > div.RnEpo._Yhr4")) {
+                        document.querySelector("body > div.RnEpo._Yhr4").remove();
+                    }
+                });
+            });
+            observer.observe(document.querySelector("body"), {
+                attributes: true,
+            });
+        })();
+    }
 }, 500);
